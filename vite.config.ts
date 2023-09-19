393451c27dbc6cf47b5429915b074e29b01818ab
Lhomme Aymeric <37447949+HeinTouchableFR@users.noreply.github.com>
2023-09-19 22:52:18 +0200

    feat: Build esNEXT

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   build: {
     manifest: true,
     minify: false,
-    target: 'es2021',
+    target: 'esNEXT',
     outDir: './public/assets',
     assetsDir: ''
   },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
 })
